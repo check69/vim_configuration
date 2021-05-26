82f50193ad4d656a91c6eaa63b992666a696973e
Antonio <antonio.checa.bustos@gmail.com>
2021-05-26 17:28:00 +0100

    Fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s configuration/.vimrc ../
-
 git clone "https://github.com/VundleVim/Vundle.vim.git" ~/.vim/bundle/Vundle.vim
 mkdir ~/.vim/.undodir
+cd ..
+ln -s .vim/configuration/.vimrc
 vim -c "PluginInstall" -c q -c q -c q ~/.vimrc
